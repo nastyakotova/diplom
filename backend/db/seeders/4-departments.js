8df4d0f085ce6df2c897d453e1420099a36c0a74
Vladimir Bogachev <bogachevvova@gmail.com>
2023-05-12 01:44:40 +0300

    Append Prepods

--- backend/db/seeders/4-departments.js
+++ backend/db/seeders/4-departments.js
@@ -24,7 +24,42 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
+          title: 'Кафедра социальной безопасности',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
           title: 'Кафедра физического воспитания и спортивно-массовой работы',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: 'Кафедра философии',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: 'Кафедра русской литературы',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: 'Кафедра истории',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: 'Кафедра английского языка для профессиональной коммуникации',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: 'Кафедра физической электроники',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: 'Кафедра информационных систем',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
